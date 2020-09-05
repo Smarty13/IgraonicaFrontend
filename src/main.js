86d82f56f82fcb0171180ba73910bc8c6833c73a
unknown <savicaleksa27@gmail.com>
2020-09-05 11:42:18 +0200

    user front edit i get by ID

--- src/main.js
+++ src/main.js
@@ -1,34 +1,33 @@
-import Vue from 'vue';
-import App from './App.vue';
-import VueRouter from 'vue-router';
-import BootstrapVue from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import Vue from "vue";
+import App from "./App.vue";
+import VueRouter from "vue-router";
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 import VueToastr from "vue-toastr";
 
+import LoginComponent from "./components/auth/LoginComponent";
+import RegisterComponent from "./components/auth/RegisterComponent";
+import SveLokacijeComponent from "./components/lokacije/SveLokacijeComponent";
+import UserComponent from "./components/user/UserComponent";
 
-
-import LoginComponent from './components/auth/LoginComponent';
-import RegisterComponent from './components/auth/RegisterComponent';
-import SveLokacijeComponent from './components/lokacije/SveLokacijeComponent';
 Vue.config.productionTip = false;
-
 
 Vue.use(VueRouter);
 Vue.use(BootstrapVue);
 Vue.use(VueToastr);
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   routes: [
-    { path: '/login', component: LoginComponent },
-    { path: '/register', component: RegisterComponent },
-    { path: '/lokacije', component: SveLokacijeComponent}
-  ]
+    { path: "/login", component: LoginComponent },
+    { path: "/register", component: RegisterComponent },
+    { path: "/lokacije", component: SveLokacijeComponent },
+    { path: "/user/:id", component: UserComponent },
+  ],
 });
-
 
 new Vue({
   router,
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
---
+++ src/services/user.service.js
@@ -0,0 +1,25 @@
+import axios from "../axios";
+import constants from "../constants";
+
+export default {
+  getUserById(id) {
+    return axios.get(`${constants.API}/user/${id}`);
+  },
+
+  getLokacijaById(id) {
+    return axios.get(`${constants.API}/lokacija/${id}`);
+  },
+
+  getPozicijaById(id) {
+    return axios.get(`${constants.API}/pozicija/${id}`);
+  },
+  getGradById(id) {
+    return axios.get(`${constants.API}/grad/${id}`);
+  },
+  putById(id, user) {
+    return axios.put(`${constants.API}/user/${id}`, user, id);
+  },
+
+  /*    fireUser(id,lokacija_id, pozicija_id) {
+        return axios.patch(`${constants.API}/user/${id}`,)*/
+};
